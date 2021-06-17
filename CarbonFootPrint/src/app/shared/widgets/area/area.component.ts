07e9bca5812bf9e1b856fbc2de23f07ff3918cf3
Susithra Chandrabose <susithra_r@yahoo.com>
2021-06-17 23:19:44 +0530

    Chart Changes

--- CarbonFootPrint/src/app/shared/widgets/area/area.component.ts
+++ CarbonFootPrint/src/app/shared/widgets/area/area.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChange, SimpleChanges } from '@angular/core';
 import * as Highcharts from 'highcharts';
 import HC_exporting from 'highcharts/modules/exporting';
 
@@ -8,19 +8,19 @@ import HC_exporting from 'highcharts/modules/exporting';
   templateUrl: './area.component.html',
   styleUrls: ['./area.component.scss']
 })
-export class AreaComponent implements OnInit {
+export class AreaComponent implements OnInit, OnChanges {
 
   chartOptions: {};
   @Input() data: any = [];
 
   Highcharts = Highcharts;
 
-  constructor() { 
-    this.chartOptions={};
+  constructor() {
+    this.chartOptions = {};
   }
 
   ngOnInit() {
-    this.chartOptions = {
+    /*this.chartOptions = {
       chart: {
         type: 'area'
       },
@@ -43,13 +43,43 @@ export class AreaComponent implements OnInit {
       series: this.data
     };
 
-    HC_exporting(Highcharts);
+    HC_exporting(Highcharts); */
 
     setTimeout(() => {
       window.dispatchEvent(
         new Event('resize')
       );
     }, 300);
+
+  }
+  ngOnChanges(change: SimpleChanges) {
+    if (change.data.currentValue && change.data.currentValue.length > 0) {
+      this.chartOptions = {
+        chart: {
+          type: 'area'
+        },
+        title: {
+          text: 'Carbon Footprint'
+        },
+        subtitle: {
+          text: 'Your annual emissions'
+        },
+        tooltip: {
+          split: true,
+          valueSuffix: ' Kgs'
+        },
+        credits: {
+          enabled: false
+        },
+        exporting: {
+          enabled: true,
+        },
+        series: change.data.currentValue
+      };
+      HC_exporting(Highcharts);
+      //Call Chart Update function here 
+    }
+    console.log(change);
   }
 
 }
